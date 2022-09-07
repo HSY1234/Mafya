ef677f479e5af83256b6e9a3a5e4573fde64472e
ParkSeHyeon <sehyeon1997@gmail.com>
2022-09-07 23:42:50 +0900

    [FEAT]fix image api with notiopn_api at imgController

--- backend/mafya/src/main/java/com/a205/mafya/api/controller/ImgController.java
+++ backend/mafya/src/main/java/com/a205/mafya/api/controller/ImgController.java
@@ -11,19 +11,47 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/img")
 public class ImgController {
 
     @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-    public ResponseEntity<?> uploadImage(@RequestPart(value = "file") MultipartFile multipartFile) {
+    public ResponseEntity<?> recognizeFace(@RequestPart(value = "file") MultipartFile multipartFile) {
         LocalTime now = LocalTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
         String formatedNow = now.format(formatter);
-        System.out.println(formatedNow + ": " + multipartFile.getContentType() + " " + multipartFile.getOriginalFilename() + " " + multipartFile.getSize());
+        System.out.println("[Face] " + formatedNow + ": " + multipartFile.getContentType() + " " + multipartFile.getOriginalFilename() + " " + multipartFile.getSize());
 
-        return (new ResponseEntity<String>("success", HttpStatus.OK));
+        int status = 0;
+        //int status = 1;   //얼굴 인식 안 됨
+        String codeName = "0743111";
+
+        Map<String, String> result = new HashMap<>();
+        result.put("status", "" + status);
+        result.put("code_name", codeName);
+
+        return (new ResponseEntity<Map<String, String>>(result, HttpStatus.OK));
     }
 
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public ResponseEntity<?> recognizeMask(@RequestPart(value = "file") MultipartFile multipartFile, String codeName) {
+        LocalTime now = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
+        String formatedNow = now.format(formatter);
+        System.out.println("[Mask] " + formatedNow + ": " + multipartFile.getContentType() + " " + multipartFile.getOriginalFilename() + " " + multipartFile.getSize());
+
+        int status = 0;
+//        int status = 1; //얼굴 인식 안 됨
+        //int status = 2; //등록된 사용자 아님
+        String name = "기사 김무종";
+
+        Map<String, String> result = new HashMap<>();
+        result.put("status", "" + status);
+        result.put("name", name);
+
+        return (new ResponseEntity<Map<String, String>>(result, HttpStatus.OK));
+    }
 }
