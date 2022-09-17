43c711bae1dcbbe81fdf8e55cf899e5c1e8170e3
HumanHyeon <sehyeon1997@gmail.com>
2022-09-17 19:05:05 +0900

    [FEAT]  modi filepath

--- backend/mafya/src/main/java/com/a205/mafya/api/controller/ImgController.java
+++ backend/mafya/src/main/java/com/a205/mafya/api/controller/ImgController.java
@@ -49,7 +49,7 @@ public class ImgController {
         boolean result =imgService.uploadCamImg(multipartFile);
 
         if (result) return (new ResponseEntity<String>(SUCCESS, HttpStatus.OK));
-        else        return (new ResponseEntity<String>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR));
+        else        return (new ResponseEntity<String>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR)); 
     }
 
     @GetMapping(value = "/{userCode}")
