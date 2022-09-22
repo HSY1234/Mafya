1663970ce810ef1279c6ac192b2d92cc51f8e960
kms940125 <kms940125@hanmail.net>
2022-09-22 23:32:52 +0900

    FE: feat enterCamera exitCamera

--- frontend/mafya/src/App.js
+++ frontend/mafya/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import Camera from "./features/webcam/camera";
+import EnterCamera from "./features/webcam/enterCamera";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import studentForm from "./features/Admin/studentForm/studentForm";
 import MainPage from "./features/Admin/mainPage/mainPage";
 import LoginPage from "./features/login/loginPage";
 import StudentMainPage from "./features/student/studentMainPage";
+import ExitCamera from "./features/webcam/exitCamera";
 import "./App.module.css";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Camera}></Route>
+          <Route exact path="/enter" component={EnterCamera}></Route>
           <Route exact path="/admin/form" component={studentForm}></Route>
           <Route exact path="/admin" component={MainPage}></Route>
           <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/exit" component={ExitCamera} />
           <Route exact path="/student" component={StudentMainPage} />
         </Switch>
       </BrowserRouter>
