ea9a4d0ea00698ac31a3b8ee869e04a5dbe3106c
KimByungSoo <jeanvaljean0@naver.com>
2022-09-26 16:34:44 +0900

    BE : refresh test

--- backend/mafya/src/main/java/com/a205/mafya/config/WebConfig.java
+++ backend/mafya/src/main/java/com/a205/mafya/config/WebConfig.java
@@ -16,10 +16,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowCredentials(true)
+                .allowedOrigins("*")
+//                .allowCredentials(true)
                 .allowedMethods("*")
-//                .allowCredentials(false)
                 .maxAge(3000)
         ;
     }
