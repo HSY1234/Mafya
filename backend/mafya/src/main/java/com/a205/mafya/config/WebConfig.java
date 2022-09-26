65a25f10439207aa9fded33e96d50dbf8b7b1868
KimByungSoo <jeanvaljean0@naver.com>
2022-09-26 14:50:24 +0900

    BE : refresh test

--- backend/mafya/src/main/java/com/a205/mafya/config/WebConfig.java
+++ backend/mafya/src/main/java/com/a205/mafya/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.a205.mafya.config;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,6 +16,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .maxAge(3000);
+    }
+
+    @Bean
+    public ServletWebServerFactory tomcatCustomizer() {
+                TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+                tomcat.addContextCustomizers(context -> context.setCookieProcessor(new LegacyCookieProcessor()));
+            return tomcat;
     }
 
 }
