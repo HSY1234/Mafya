20fcf5a8a9081519f6f4f0d57f5611c4b1eb65ed
ParkSeHyeon <sehyeon1997@gmail.com>
2022-10-05 11:43:13 +0900

    [FEAT] modi manager

--- backend/mafya/src/main/java/com/a205/mafya/api/service/ManagerServiceimpl.java
+++ backend/mafya/src/main/java/com/a205/mafya/api/service/ManagerServiceimpl.java
@@ -6,6 +6,7 @@ import com.a205.mafya.db.repository.ManagerRepository;
 import com.a205.mafya.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +15,8 @@ public class ManagerServiceimpl implements ManagerService{
     @Autowired
     ManagerRepository managerRepository;
 
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @Override
@@ -24,7 +25,7 @@ public class ManagerServiceimpl implements ManagerService{
                 .name(managerReq.getName())
                 .managerCode(managerReq.getManagerCode())
                 .classCode(managerReq.getClassCode())
-                .password(managerReq.getPassword())
+                .password(passwordEncoder.encode(managerReq.getPassword()))
                 .build();
 
         managerRepository.save(manager);
